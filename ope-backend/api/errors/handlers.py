cbb2029526aedfddb5972780b0b3d89826f63ffa
Matheus Teixeira <matheus.teixeira@itenabler.com.br>
2021-08-13 09:26:49 -0300

    Arquitetura backend base

---
+++ ope-backend/api/errors/handlers.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+
+errors = Blueprint('errors', __name__)
+
+
+@errors.app_errorhandler(404)
+def page_not_found(error):
+    return 404
+
+
+@errors.app_errorhandler(403)
+def access_denied(error):
+    return 403
+
+
+@errors.app_errorhandler(500)
+def internal_error(error):
+    return 500
