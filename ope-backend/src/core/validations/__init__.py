63b06f7acbbf6d0562f786d03eb5f57db659bc0c
Alan Poveda <alsepoveda@gmail.com>
2021-08-23 22:18:09 -0300

    Criação do método GET BY ID da tabela asserts

--- ope-backend/src/core/validations/__init__.py
+++ ope-backend/src/core/validations/__init__.py
@@ -3,3 +3,4 @@ from .user_validations.get_user_by_id_validation import get_user_validation
 from .drink_validations.create_drink_validation import create_drink_validation
 from .dessert_validations.create_dessert_validation import create_dessert_validation
 from .role_validations.create_role_validation import create_role_validation
+from .dessert_validations.get_dessert_by_id_validation import get_dessert_validation
