17980f9d22dcfa5a3852bc52b8cf92dc2fafc5b5
Matheus Teixeira <matheus.teixeira@itenabler.com.br>
2021-08-24 10:51:11 -0300

    Correção no get dessert by id

--- ope-backend/src/infra/repository/dessert_repository.py
+++ ope-backend/src/infra/repository/dessert_repository.py
@@ -2,19 +2,20 @@ from sqlalchemy.exc import IntegrityError, NoResultFound, MultipleResultsFound
 from src.infra.config import DBConnectionHandler
 from src.infra.db_entities import Desserts as Dessert
 
+
 class DessertRepository:
 
     @classmethod
     def create_dessert(cls, name: str, description: str, price: float, amount: int, img: str):
         with DBConnectionHandler() as db:
             try:
-                new_dessert = Dessert(name=name, description=description ,price=price, amount=amount, img=img)
+                new_dessert = Dessert(name=name, description=description, price=price, amount=amount, img=img)
                 db.session.add(new_dessert)
                 db.session.commit()
                 return {"data": new_dessert.to_dict(), "status": 201, "errors": []}
             except IntegrityError:
                 db.session.rollback()
-                return {"data": None, "status": 409, "errors":["Erro na requisição"]}
+                return {"data": None, "status": 409, "errors": ["Erro na requisição"]}
             except Exception as ex:
                 print(ex)
                 db.session.rollback()
@@ -42,26 +43,26 @@ class DessertRepository:
                 db.session.close()
 
     @classmethod
-    def get_dessert_by_id(cls, dessert_id:int):
+    def get_dessert_by_id(cls, dessert_id: int):
         with DBConnectionHandler() as db:
             try:
                 dessert = db.session.query(Dessert).filter_by(id=dessert_id).one()
                 return {
-                        "data": dessert.to_dict(),
-                        "status": 200,
-                        "errors": [f"Sobremesa de nome {dessert_id} não existe"]
+                    "data": dessert.to_dict(),
+                    "status": 200,
+                    "errors": []
                 }
             except NoResultFound:
                 return {
                     "data": None,
                     "status": 404,
-                    "errors": [f"Sobremesa de nome {dessert_id} não existe"]
+                    "errors": [f"Sobremesa de id {dessert_id} não existe"]
                 }
             except MultipleResultsFound:
                 return {
                     "data": None,
                     "status": 409,
-                    "errors": [f"Conflito de usuário com id {dessert_id}"]
+                    "errors": [f"Conflito de sobremesa com id {dessert_id}"]
                 }
             except Exception as ex:
                 return {
