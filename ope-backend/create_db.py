a156fda66f9aa14296f67c4236136015fb0a36c4
mhora <56177654+mthora@users.noreply.github.com>
2021-12-10 11:31:31 -0300

    fix backend env variable

--- ope-backend/create_db.py
+++ ope-backend/create_db.py
@@ -5,14 +5,3 @@ from src.main.utils import hash_password
 db_conn = DBConnectionHandler()
 engine = db_conn.get_engine()
 Base.metadata.create_all(engine)
-
-with DBConnectionHandler() as db:
-    try:
-        new_user = Users(name='admin', role=1, email='admin@suzy.com', password=hash_password('admin'))
-        db.session.add(new_user)
-        db.session.commit()
-    except Exception as ex:
-        print(ex)
-        db.session.rollback()
-    finally:
-        db.session.close()
